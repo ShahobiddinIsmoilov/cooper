ee638bcfd4492427f45bf16983c21d195d507904
triton <shahobiddinismoilov@gmail.com>
2024-04-24 02:21:03 +0500

    i don't know wtf is happening

---
+++ cooper/mainview.py
@@ -0,0 +1,9 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+
+@api_view(['GET'])
+def OK(request):
+    return Response("HALF-LIFE 2 IS THE BEST GAME EVER MADE", status=status.HTTP_200_OK)
--- cooper/urls.py
+++ cooper/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .mainview import OK
 
 
 urlpatterns = [
+    path('/', OK, 'OK'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
