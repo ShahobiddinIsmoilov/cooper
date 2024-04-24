198a6436602dea307692d7c1d7920c6d3b1bdd21
triton <shahobiddinismoilov@gmail.com>
2024-04-24 21:28:11 +0500

    set permissions to is authenticated or read only

--- cooper/settings.py
+++ cooper/settings.py
@@ -207,6 +207,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     )
